82d8e98233b1ce6295e89947f9d797018bd8f8f2
karl-no <karl.publicidade@gmail.com>
2022-12-15 14:44:27 -0300

    Requisito 2

--- src/models/product.model.ts
+++ src/models/product.model.ts
@@ -1,4 +1,4 @@
-import { Pool, ResultSetHeader } from 'mysql2/promise';
+import { Pool, ResultSetHeader, RowDataPacket } from 'mysql2/promise';
 import Product from '../interfaces/product.interface';
 
 export default class ProductModel {
@@ -17,5 +17,13 @@ export default class ProductModel {
     const [dataInserted] = result;
     const { insertId } = dataInserted;
     return { id: insertId, ...product };
+  }
+
+  public async getAll(): Promise<Product[]> {
+    const result = await this.connection.execute<RowDataPacket[] & Product[]>(
+      'SELECT * FROM Trybesmith.products',
+    );
+    const [rows] = result;
+    return rows as Product[];
   }
 }
